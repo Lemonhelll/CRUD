d741afc1bfdb3f71236589eea0d1ac6c6d301b58
Vladimir Zolotov <volodya.zolotov.14@mail.ru>
2022-11-07 20:24:57 +0300

    rename methods

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -24,42 +24,41 @@ public class UserController {
     }
 
     @GetMapping("/")
-    public String getUserList(Model model) {
+    public String showAllUsers(Model model) {
         List<User> users = userService.getAllUsers();
         model.addAttribute("users", users);
         return "users/index";
     }
 
     @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model) {
+    public String showOneUser(@PathVariable("id") int id, Model model) {
         model.addAttribute("user", userService.getById(id));
         return "users/show";
     }
 
     @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user, Model model) {
+    public String createUser(@ModelAttribute("user") User user, Model model) {
         return "users/new";
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+    public String addUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "users/new";
         }
-        System.out.println(user.getName());
         userService.add(user);
         return "redirect:/users/";
     }
 
     @GetMapping("/{id}/edit")
-    public String edit(@PathVariable("id") int id, Model model) {
+    public String editUser(@PathVariable("id") int id, Model model) {
         model.addAttribute("user", userService.getById(id));
         return "users/edit";
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") @Valid User user,
-                         BindingResult bindingResult, @PathVariable int id) {
+    public String updateUser(@ModelAttribute("user") @Valid User user,
+                             BindingResult bindingResult, @PathVariable int id) {
         if (bindingResult.hasErrors()) {
             return "users/edit";
         }
@@ -68,7 +67,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
+    public String deleteUser(@PathVariable("id") int id) {
         userService.delete(id);
         return "redirect:/users/";
     }
